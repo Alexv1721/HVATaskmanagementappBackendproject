07e02abb3a2e3f7d23c0b80fafc6d3da6f6c742a
Alexv1721 <alexv.ug22.cs@francisxavier.ac.in>
2025-12-23 20:49:36 +0530

    Fix: Deployment

--- frontend/taskmanagementapp/src/App.js
+++ frontend/taskmanagementapp/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Tasks from './components/Tasks';
 import Login from './components/Login';
 import Register from './components/Register';
-
+import Admin from './/components/Admin'
 function App() {
   return (
     <BrowserRouter>
@@ -11,7 +11,7 @@ function App() {
   <Routes>
 
     <Route path='/' element={<Tasks/>}/>
-        <Route path='/admin' element={<Tasks/>}/>
+        <Route path='/admin' element={<Admin/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='register' element={<Register/>}/>
   </Routes>
